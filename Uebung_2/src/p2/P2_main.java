5eabd112abc4394fbdb2f85d2210225d6c9b8518
Felix Hattrup <felix@MacBook-Air-von-Felix.local>
2025-10-28 10:47:17 +0100

    Korrektur 2 und 3. Präsenz Aufgabe

--- Uebung_2/src/p2/P2_main.java
+++ Uebung_2/src/p2/P2_main.java
@@ -1,4 +1,15 @@
 package p2;
 
 public class P2_main {
+    public static void main() {
+        int i = 4;
+        boolean b = false;
+
+        if (i > 10) {
+            b = true;
+        }
+
+        System.out.println(b);
+        System.out.println(i);
+    }
 }
--- Uebung_2/src/p3/P3_main.java
+++ Uebung_2/src/p3/P3_main.java
@@ -1,4 +1,19 @@
 package p3;
 
 public class P3_main {
+    public static void main() {
+        int i = 1;
+        int j = 5;
+        int k = 8;
+
+        if (i < j) {
+            System.out.println("A");
+        }
+        if (i < k) {
+            System.out.println("B");
+        }
+        if (j < k) {
+            System.out.println("C");
+        }
+    }
 }
